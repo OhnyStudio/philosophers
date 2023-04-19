c674b25f7df5f06888e669aa204b55e822b82559
Johny <jsavard@c2r6p10.42quebec.com>
2023-04-19 14:24:54 -0400

    mutex et thread created

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,21 +6,30 @@
 /*   By: jsavard <jsavard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 11:27:14 by jsavard           #+#    #+#             */
-/*   Updated: 2023/04/19 12:22:17 by jsavard          ###   ########.fr       */
+/*   Updated: 2023/04/19 13:44:09 by jsavard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
-# include "stdio.h"
+
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdbool.h>
+# include <sys/time.h>
+# include <limits.h>
 
 typedef struct s_philo_struct
 {
-	long	nb_philo;
-	long	time_to_die;
-	long	time_to_eat;
-	long	time_to_sleep;
-	long	how_many_eat;
+	long			nb_philo;
+	long			time_to_die;
+	long			time_to_eat;
+	long			time_to_sleep;
+	long			how_many_eat;
+	pthread_t		*philosophers;
+	pthread_mutex_t	*forks;
 }	t_philo_struct;
 
 int		all_argv_int(char **argv, int argc);
--- philo/src/philo.c
+++ philo/src/philo.c
@@ -6,7 +6,7 @@
 /*   By: jsavard <jsavard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 10:53:51 by jsavard           #+#    #+#             */
-/*   Updated: 2023/04/19 12:25:32 by jsavard          ###   ########.fr       */
+/*   Updated: 2023/04/19 13:42:26 by jsavard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,15 @@ void	set_data(t_philo_struct *philo_struct, char **argv, int argc)
 		philo_struct->how_many_eat = -1;
 }
 
+static void	*routine()
+{
+	return (0);
+}
+
 int	main(int argc, char **argv)
 {
 	t_philo_struct	philo_struct;
+	int				i;
 
 	if ((argc < 5 || argc > 6) || all_argv_int(argv, argc) == 0)
 		printf("Mauvais parametres !");
@@ -35,7 +41,27 @@ int	main(int argc, char **argv)
 		set_data(&philo_struct, argv, argc);
 		if (validate_int(&philo_struct) != 0)
 		{
-			//Do thing
+			i = 0;
+			philo_struct.philosophers = (pthread_t *)malloc(sizeof(pthread_t)
+					* philo_struct.nb_philo);
+			philo_struct.forks = (pthread_mutex_t *)malloc(sizeof
+					(pthread_mutex_t) * philo_struct.nb_philo);
+			while (i < philo_struct.nb_philo)
+			{
+				pthread_mutex_init(&philo_struct.forks[i], NULL);
+				if (pthread_create(&philo_struct.philosophers[i],
+						NULL, &routine, NULL) != 0)
+					return (1);
+				i++;
+			}
+			i = 0;
+			while (i < philo_struct.nb_philo)
+			{
+				if (pthread_join(philo_struct.philosophers[i], NULL) != 0)
+					return (2);
+				pthread_mutex_destroy(&philo_struct.forks[i]);
+				i++;
+			}
 		}
 	}
 	return (0);
